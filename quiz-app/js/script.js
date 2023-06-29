b3fc4fc58096bfd6cf9205cf8d79cbbcfdebe214
Miles Pieterse <milesyusuf123@gmail.com>
2023-06-29 16:06:14 +0200

    Created Project Files

--- quiz-app/js/script.js
+++ quiz-app/js/script.js
@@ -1,100 +1,114 @@
 // OBJECTS
 
-const questions = {
-  questionOne: [
-    {
-      question:
-        "Elliot Alderson, the main character suffers from a mental health disorder. What is it?",
-      image: "/images/elliot.jpg",
-      choices: [
-        "Social anxiety disorder",
-        "Bipolar disorder",
-        "Dissociative identity disorder",
-        "Obsessive-compulsive disorder",
-      ],
-      correctAnswer: "Dissociative identity disorder",
-    },
-  ],
+const questions = [
+  {
+    question:
+      "Elliot Alderson, the main character suffers from a mental health disorder. What is it?",
+    image: "/images/elliot.jpg",
+    choices: [
+      "Social anxiety disorder",
+      "Bipolar disorder",
+      "Dissociative identity disorder",
+      "Obsessive-compulsive disorder",
+    ],
+    correctAnswer: "Dissociative identity disorder",
+  },
 
-  questionTwo: [
-    {
-      question:
-        "Darlene Alderson, Elliot's sister, is a skilled hacker. What is her hacker alias?",
-      image: "/images/darlene.jpg",
-      choices: [
-        "The Mistress",
-        "The Puppetmaster",
-        "The Architect",
-        "The Queen of Hearts",
-      ],
-      correctAnswer: "The Mistress",
-    },
-  ],
+  {
+    question:
+      "Darlene Alderson, Elliot's sister, is a skilled hacker. What is her hacker alias?",
+    image: "/images/darlene.jpg",
+    choices: [
+      "The Mistress",
+      "The Puppetmaster",
+      "The Architect",
+      "The Queen of Hearts",
+    ],
+    correctAnswer: "The Mistress",
+  },
 
-  questionThree: [
-    {
-      question:
-        "Angela Moss, Elliot's childhood friend, works at the same corporation as Elliot. What is the name of that corporation?",
-      image: "/images/angela.jpg",
-      choices: ["All Safe", "E Corp", "Evil Corp", "F Society"],
-      correctAnswer: "All Safe",
-    },
-  ],
+  {
+    question:
+      "Angela Moss, Elliot's childhood friend, works at the same corporation as Elliot. What is the name of that corporation?",
+    image: "/images/angela.jpg",
+    choices: ["All Safe", "E Corp", "Evil Corp", "F Society"],
+    correctAnswer: "All Safe",
+  },
 
-  questionFour: [
-    {
-      question:
-        "Elliot's protector identity takes the form of his deceased father. What is his father's name?",
-      image: "/images/protector.jpg",
-      choices: [
-        "Edward Alderson",
-        "Robert Alderson",
-        "William Alderson",
-        "Jacob Alderson",
-      ],
-      correctAnswer: "Edward Alderson",
-    },
-  ],
+  {
+    question:
+      "Elliot's protector identity takes the form of his deceased father. What is his father's name?",
+    image: "/images/protector.jpg",
+    choices: [
+      "Edward Alderson",
+      "Robert Alderson",
+      "William Alderson",
+      "Jacob Alderson",
+    ],
+    correctAnswer: "Edward Alderson",
+  },
 
-  questionFive: [
-    {
-      question:
-        "Tyrell Wellick, a high-ranking executive at E Corp, becomes involved with F Society. What is Tyrell's ultimate goal?",
-      image: "/images/tyrell.jpg",
-      choices: [
-        "Becoming CEO of E Corp",
-        "Toppling the government",
-        "Starting a global revolution",
-        " Destroying F Society",
-      ],
-      correctAnswer: " Becoming CEO of E Corp",
-    },
-  ],
+  {
+    question:
+      "Tyrell Wellick, a high-ranking executive at E Corp, becomes involved with F Society. What is Tyrell's ultimate goal?",
+    image: "/images/tyrell.jpg",
+    choices: [
+      "Becoming CEO of E Corp",
+      "Toppling the government",
+      "Starting a global revolution",
+      " Destroying F Society",
+    ],
+    correctAnswer: " Becoming CEO of E Corp",
+  },
 
-  questionSix: [
-    {
-      question:
-        "Whiterose is a mysterious character who leads the Dark Army. What is Whiterose's true identity?",
-      image: "/images/whiterose.jpg",
-      choices: [
-        "A transgender woman",
-        "A high-ranking government official",
-        "A master hacker",
-        "A ghost in the machine",
-      ],
-      correctAnswer: "A transgender woman",
-    },
-  ],
+  {
+    question:
+      "Whiterose is a mysterious character who leads the Dark Army. What is Whiterose's true identity?",
+    image: "/images/whiterose.jpg",
+    choices: [
+      "A transgender woman",
+      "A high-ranking government official",
+      "A master hacker",
+      "A ghost in the machine",
+    ],
+    correctAnswer: "A transgender woman",
+  },
 
-  questionSeven: [
-    {
-      question:
-        "Dominique 'Dom' DiPierro is an FBI agent investigating the cyber attacks orchestrated by F Society. Which season does Dom first appear in?",
-      image: "/images/dominique.jpg",
-      choices: ["Season 1", "Season 2", "Season 3", "Season 4"],
-      correctAnswer: "Season 2",
-    },
-  ],
-};
+  {
+    question:
+      "Dominique 'Dom' DiPierro is an FBI agent investigating the cyber attacks orchestrated by F Society. Which season does Dom first appear in?",
+    image: "/images/dominique.jpg",
+    choices: ["Season 1", "Season 2", "Season 3", "Season 4"],
+    correctAnswer: "Season 2",
+  },
+];
+// };
 
 // OBJECTS END
+
+// VARIABLE REFERENCES
+const quizContainer = document.querySelector(".quiz-container");
+const questionContainer = document.querySelector(".question-container");
+const nextQuestionBtn = document.querySelector(".next-question-btn");
+// VARIABLE REFERENCES END
+
+// FUNCTIONALITY
+
+function createQuestionCard(data) {
+  data.forEach((question) => {
+    const questionBody = document.createElement("div");
+    questionBody.innerHTML = `
+     <div class="question">${question.question}</div>
+     <div class="image">${question.image}</div>
+     `;
+     return questionBody;
+    // questionContainer.appendChild(questionBody);
+  });
+}
+
+// let staticContent = createQuestionCard(questions);
+
+// questionContainer.appendChild(staticContent);
+
+// console.log(staticContent);
+// FUNCTIONALITY END
